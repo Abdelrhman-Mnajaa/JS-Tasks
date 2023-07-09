97f41b21451350b9ccc6e55bb643275d6678ddd8
Abdelrhman-Mnajaa <abdelrhmanmnajaa@gmail.com>
2023-07-09 14:40:48 +0300

    Update

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let x,y,z;
+
+x=Number(prompt("Please enter the first number"));
+y=Number(prompt("Please enter the 2nd number"));
+z=Number(prompt("Please enter the 3rd number"));
+
+console.log('The Summation is ', x+y+z);
